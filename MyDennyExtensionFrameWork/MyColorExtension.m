6ad209cb10390effa3acb8052b18b28aa957feac
Denny <denny@nj-dennis-jw-mac.local>
2017-05-11 14:03:35 +0800

    Test

--- MyDennyExtensionFrameWork/MyColorExtension.m
+++ MyDennyExtensionFrameWork/MyColorExtension.m
@@ -12,6 +12,7 @@
 
 + (UIColor *)colorWithHexString:(NSString *)color alpha:(CGFloat)alpha
 {
+    
     //删除字符串中的空格
     NSString *cString = [[color stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
     // String should be 6 or 8 characters
